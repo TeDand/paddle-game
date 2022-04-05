86d118b16c2ca2c0d20962494f0b005e067836ce
TeDand <tejas-d@hotmail.com>
2022-04-05 19:27:37 +0100

    Added you won screen

--- src/game.js
+++ src/game.js
@@ -9,6 +9,7 @@ const GAMESTATE = {
   MENU: 2,
   GAMEOVER: 3,
   NEWLEVEL: 4,
+  COMPLETED: 5,
 }
 
 export default class Game {
@@ -16,20 +17,23 @@ export default class Game {
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
     this.gameState = GAMESTATE.MENU;
-    this.lives = 3;
+    this.maxLives = 3;
+    this.lives = this.maxLives;
     this.paddle = new Paddle(this);
     this.ball = new Ball(this);
     this.bricks = [];
     this.gameObjects = [];
     this.levels = [level1, level2];
     this.currentLevel = 0;
+    this.maxLevels = 2;
     new InputHandler(this);
   }
 
   start() {
     if (this.gameState !== GAMESTATE.MENU &&
       this.gameState !== GAMESTATE.NEWLEVEL &&
-      this.gameState !== GAMESTATE.GAMEOVER) return;
+      this.gameState !== GAMESTATE.GAMEOVER &&
+      this.gameState !== GAMESTATE.COMPLETED) return;
 
     this.bricks = buildLevel(this, this.levels[this.currentLevel]);
     this.ball.reset();
@@ -41,19 +45,26 @@ export default class Game {
     if (this.lives === 0) {
       this.gameState = GAMESTATE.GAMEOVER;
       this.currentLevel = 0;
-      this.lives = 3;
+      this.lives = this.maxLives;
     }
 
     if (this.gameState === GAMESTATE.PAUSED ||
       this.gameState === GAMESTATE.MENU ||
-      this.gameState === GAMESTATE.GAMEOVER)
+      this.gameState === GAMESTATE.GAMEOVER ||
+      this.gameState === GAMESTATE.COMPLETED)
       return;
 
-    // TODO: Create winning state/screen
     if (this.bricks.length === 0) {
       this.currentLevel++;
+      this.lives = this.maxLives;
       this.gameState = GAMESTATE.NEWLEVEL;
       this.start();
+    }
+
+    if (this.currentLevel === this.maxLevels) {
+      this.gameState = GAMESTATE.COMPLETED;
+      this.lives = this.maxLives;
+      this.currentLevel = 0;
     }
 
     [...this.gameObjects, ...this.bricks].forEach((object) => {
@@ -99,6 +110,18 @@ export default class Game {
       ctx.fillStyle = "white";
       ctx.textAlign = "center";
       ctx.fillText("GAME OVER", this.gameWidth / 2, this
+        .gameHeight / 2);
+      ctx.fillText("Press SPACEBAR to re-start", this.gameWidth / 2, this
+        .gameHeight / 2 + 35);
+    }
+
+    if (this.gameState === GAMESTATE.COMPLETED) {
+      ctx.fillStyle = "rgba(0,0,0,1)";
+      ctx.fillRect(0, 0, this.gameWidth, this.gameHeight);
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "white";
+      ctx.textAlign = "center";
+      ctx.fillText("YOU WON!", this.gameWidth / 2, this
         .gameHeight / 2);
       ctx.fillText("Press SPACEBAR to re-start", this.gameWidth / 2, this
         .gameHeight / 2 + 35);
